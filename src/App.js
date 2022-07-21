468eaf033ba63e981298b4b5f8acdae5018ba972
Vishal Arora <vishalarora@Vishal-Macbook.local>
2022-07-21 13:35:20 +0800

    PKIJS Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+// MyApp.js
+import React, { lazy } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const pkijs = require("pkijs");
+console.log(pkijs);
+const crypto = pkijs.getCrypto(true);
+
+const algorithm = pkijs.getAlgorithmParameters("RSASSA-PKCS1-V1_5", "generateKey");
+const pair = crypto.generateKey(algorithm.algorithm, true, ["sign", "verify"]);// as Required<CryptoKeyPair>;
+pair.then(keys => console.log(keys));
 function App() {
   return (
     <div className="App">
